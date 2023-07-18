631bd8701adc64f8189b3fd9c470abf5ec48cab8
kowshik <93364990+Kowshik2001@users.noreply.github.com>
2023-07-18 09:50:33 +0530

    Binary search tree and traversal methods

---
+++ Day 6/Preorder traversal.c
@@ -0,0 +1,54 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+struct node {
+	int data;
+	struct node* left;
+	struct node* right;
+};
+
+
+struct node* newNode(int data)
+{
+	struct node* node
+		= (struct node*)malloc(sizeof(struct node));
+	node->data = data;
+	node->left = NULL;
+	node->right = NULL;
+
+	return (node);
+}
+
+void printPreorder(struct node* node)
+{
+	if (node == NULL)
+		return;
+
+	
+	printf("%d ", node->data);
+
+	
+	printPreorder(node->left);
+
+	
+	printPreorder(node->right);
+}
+
+
+int main()
+{
+	struct node* root = newNode(1);
+	root->left = newNode(2);
+	root->right = newNode(3);
+	root->left->left = newNode(4);
+	root->left->right = newNode(5);
+
+	// Function call
+	printf("Preorder traversal of binary tree is \n");
+	printPreorder(root);
+
+	getchar();
+	return 0;
+}
